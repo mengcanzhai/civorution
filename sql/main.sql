34b2fc69a01b209c370f89e9457fc2c70fb0c059
mengcanzhai <mengcanzhai@outlook.com>
2019-02-02 01:43:07 +0800

    提交sql

--- sql/main.sql
+++ sql/main.sql
@@ -1,10 +1,14 @@
+/*user table for login/up*/
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
+  userid int AUTO_INCREMENT,
   `user` varchar(30) DEFAULT NULL,
-  `pswd` varchar(32) DEFAULT NULL,
-  `userid` 
+  pswd varchar(32) DEFAULT NULL,
+  primary key(userid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `user` WRITE;
-INSERT INTO `user` VALUES ('mc','e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` VALUES (,'mc','e10adc3949ba59abbe56e057f20f883e');
 UNLOCK TABLES;
+
+/*u*/
